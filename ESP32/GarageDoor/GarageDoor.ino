b3ef42a18d1178ca3c4717875056a1176f29f0f2
Jeremiah Boyle <jeremiah.boyle@gmail.com>
2021-03-04 22:56:26 -0800

    Async webserver

--- ESP32/GarageDoor/GarageDoor.ino
+++ ESP32/GarageDoor/GarageDoor.ino
@@ -1,4 +1,6 @@
+#include <AsyncTCP.h>
 #include <Bounce2.h>
+#include <ESPAsyncWebServer.h>
 #include <RTClib.h>
 #include <TimerEvent.h>
 #include <WiFi.h>
@@ -25,18 +27,24 @@ Bounce reedSwitch;
 
 TimerEvent timer;
 wl_status_t wifiStatus;
+AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  led.init(5);
   setupRTC();
+
+  led.init(5);
   lcd.init();
+  lcd.sleepAfter(1000 * 60 * 3);
   lcd.backlight(true);
   timer.set(1000, updateDisplay);
   updateDisplay();
+
   reedSwitch.attach(REED_SWITCH_PIN, INPUT_PULLUP);
   reedSwitch.interval(25);
+
   WiFi.begin(ssid, password);
+  setupServer();
 }
 
 void loop() {
@@ -66,6 +74,77 @@ void setupRTC() {
     println("RTC lost power; setting time");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+}
+
+void setupServer() {
+  const char *index_html = R"(
+      <head><meta http-equiv="refresh" content="5"></head>
+      <h2>
+      Garage is %door%<br>
+      <p>
+      Toggle <a href="/activate">Garage Opener</a><br>
+      <p>
+      Toggle <a href="/backlight">Backlight</a><br>
+      <p>
+      %ssid% (%rssi% dBm)<br>
+      <p>
+      %now%
+  )";
+
+  server.on("/", HTTP_GET, [index_html](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html, templateVar);
+  });
+
+  server.on("/backlight", HTTP_GET, [](AsyncWebServerRequest *request) {
+    lcd.toggleBacklight();
+    request->redirect("/");
+  });
+
+  server.on("/activate", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // TODO toggle door opener
+    request->redirect("/");
+  });
+
+  server.on("/api/status", HTTP_GET, [index_html](AsyncWebServerRequest *request) {
+    request->send_P(200, "application/json",
+                    R"({"door": "%door%", "timestamp": %timestamp%, "rssi": %rssi%})",
+                    templateVar);
+  });
+
+  server.on("/api/backlight", HTTP_GET, [](AsyncWebServerRequest *request) {
+    lcd.toggleBacklight();
+    request->send(200, "application/json", R"({"status": "ok"})");
+  });
+
+  server.on("/api/activate", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // TODO toggle door opener
+    request->send(200, "application/json", R"({"status": "ok"})");
+  });
+
+  server.begin();
+}
+
+String templateVar(const String& var){
+  if (var == "door") {
+    return digitalRead(REED_SWITCH_PIN) == LOW ? "closed" : "open";
+  }
+  else if (var == "ssid") {
+    return WiFi.SSID();
+  }
+  else if (var == "rssi") {
+    return String(WiFi.RSSI());
+  }
+  else if (var == "timestamp") {
+    return String(rtc.now().unixtime());
+  }
+  else if (var == "now") {
+    char buf[20];
+    DateTime now = rtc.now();
+    snprintf(buf, sizeof(buf), "%s %02d:%02d:%02d",
+             DOW[now.dayOfTheWeek()], now.hour(), now.minute(), now.second());
+    return String(buf);
+  }
+  return "";
 }
 
 void updateDisplay() { // called once per second
