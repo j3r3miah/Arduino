433b8b8f9222942bdc20442b56cee5864e1e683a
Jeremiah Boyle <jeremiah.boyle@gmail.com>
2021-03-04 20:07:00 -0800

    Improve Lcd helper

--- ESP32/GarageDoor/Lcd.h
+++ ESP32/GarageDoor/Lcd.h
@@ -2,17 +2,20 @@
 #include <Print.h>
 #include <LiquidCrystal_I2C.h>
 
+#define ROWS 4
+#define COLS 20
+
 class Lcd {
 private:
   LiquidCrystal_I2C lcd;
   unsigned long sleepAfterMillis = 0;
   unsigned long activatedMillis = 0;
   bool backlightEnabled = false;
-  char buf[20] = {0};
-  char buf2[20] = {0};
+  char buf[COLS] = {0};
+  char buf2[COLS] = {0};
 
 public:
-  Lcd() : lcd(LiquidCrystal_I2C(0x27, 20, 4)) { }
+  Lcd() : lcd(LiquidCrystal_I2C(0x27, COLS, ROWS)) { }
 
   void init() {
     lcd.init();
@@ -30,44 +33,6 @@ public:
     sleepAfterMillis = millis;
   }
 
-  void print(int row, String msg) {
-    sprintf(buf, "%-20s", msg.c_str());
-    lcd.setCursor(0, row);
-    lcd.print(buf);
-  }
-
-  void print(int row, int col, String msg) {
-    lcd.setCursor(col, row);
-    lcd.print(msg);
-  }
-
-  void printf(int row, int col, char *fmt, ...) {
-    va_list args;
-    va_start (args, fmt );
-    vprintf(row, col, fmt, args);
-    va_end (args);
-  }
-
-  void printf(int row, char *fmt, ...) {
-    va_list args;
-    va_start (args, fmt );
-    vprintf(row, 0, fmt, args);
-    va_end (args);
-  }
-
-  void vprintf(int row, int col, char *fmt, va_list args) {
-    vsnprintf(buf, sizeof(buf), fmt, args);
-    // pad to 20 chars (clear to end of line)
-    snprintf(buf2, sizeof(buf2), "%-20s", buf);
-    lcd.setCursor(col, row);
-    lcd.print(buf2);
-  }
-
-  void clear(int row) {
-    lcd.setCursor(0, row);
-    lcd.print("                    ");
-  }
-
   void backlight(bool enable) {
     backlightEnabled = enable;
     lcd.setBacklight(enable);
@@ -76,6 +41,45 @@ public:
 
   void toggleBacklight() {
     backlight(!backlightEnabled);
+  }
+
+  void printf(int row, int col, const char *fmt, ...) {
+    va_list args;
+    va_start (args, fmt );
+    vprintf(row, col, fmt, args);
+    va_end (args);
+  }
+
+  void printf(int row, const char *fmt, ...) {
+    va_list args;
+    va_start (args, fmt );
+    vprintf(row, 0, fmt, args);
+    va_end (args);
+  }
+
+  void vprintf(int row, int col, const char *fmt, va_list args) {
+    vsnprintf(buf, sizeof(buf), fmt, args);
+    // pad with spaces to end of line
+    snprintf(buf2, sizeof(buf2), "%-*s", COLS - col, buf);
+    lcd.setCursor(col, row);
+    lcd.print(buf2);
+  }
+
+  void print(int row, String msg) {
+    print(row, 0, msg);
+  }
+
+  void print(int row, int col, String msg) {
+    // pad with spaces to end of line
+    snprintf(buf2, sizeof(buf2), "%-*s", COLS - col, msg.c_str());
+    lcd.setCursor(col, row);
+    lcd.print(buf2);
+  }
+
+  void clear(int row) {
+    snprintf(buf2, sizeof(buf2), "%-*s", COLS, "");
+    lcd.setCursor(0, row);
+    lcd.print(buf2);
   }
 
   Print *getPrinter(int col, int row) {
